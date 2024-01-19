7ccb39c4f68059cb0605f45931d9c8e2f541cd29
id-edm <ivandubochit@gmail.com>
2024-01-19 19:38:23 +0400

    feat: add solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+readStream.on('data', (chunk) => {
+  console.log(chunk);
+});
+
+readStream.on('end', () => {
+  readStream.close(); 
+});
+
+readStream.on('error', (err) => {
+  console.error(`An error occurred while reading the file: ${err.message}`);
+});
