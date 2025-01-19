f67b193c1d523b38f139c3943a5dea7d96c30e44
id-edm <ivandubochit@gmail.com>
2025-01-19 18:08:13 +0400

    feat: ready solutions 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const route = path.join(__dirname, '02-write-file.txt');
+
+const { stdin: input, stdout: output } = require('process');
+
+const rl = readline.createInterface({ input, output });
+console.log('Enter text:');
+
+rl.on('line', (input) => {
+	if (input) {
+		if (input.toLowerCase().trim() === 'exit') {
+			console.log('Text entry complete!');
+			process.exit();
+		} else {
+			fs.appendFile(route, `${input}\n`, (err) => {
+				if (err) {
+					console.error(`Error adding to file: ${err}`);
+				}
+			});
+		}
+	}
+});
+
+rl.on('close', () => {
+	console.log('Text entry complete!');
+});
